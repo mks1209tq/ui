57516b886447a5eceff6d613adac85e09f01557b
Mohammed Maroof <19404@TQ.INV>
2024-11-01 10:54:32 +0400

    ui changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,12 @@
 <x-app-layout>
-asdf
+<main class="xl:pl-6">
+      <div class="px-4 py-10 sm:px-6 lg:px-8 lg:py-6 ml-12 mr-6 my-6 border-2 border-red-500">
+        <!-- Main area -->
+         Hello  
+      </div>
+    </main>
+  </div>
+
+  
+</div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-white">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,29 +12,11 @@
         
     </head>
 
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        
-    
-    @include('layouts.header')
-    <div class="mx-auto w-full max-w-7xl grow lg:flex xl:px-2">
-    <!-- Left sidebar & main wrapper -->
-    <div class="flex-1 xl:flex">
-      <div class="border-b border-gray-200 px-4 py-6 sm:px-6 lg:pl-8 xl:w-64 xl:shrink-0 xl:border-b-0 xl:border-r xl:pl-6">
-        <!-- Left column area -->
-        @include('layouts.navigation')
-      </div>
-
-      <div class="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-        <!-- Main area -->
-         asdf
-      </div>
-    </div>
-
-    
-  </div>
-</div>
-
-    
+    <body class="font-sans antialiased dark:bg-black dark:text-white/50 h-full">
+      @include('layouts.navigation')
+      {{ $slot }}
+     
+   
     </body>
 
 </html>
